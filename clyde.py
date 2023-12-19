6fd7d93dd565f49453a5d270f294933980a2e7b9
lun4h <71488561+piotr25691@users.noreply.github.com>
2023-12-19 10:36:11 +0100

    you don't need a while True since bing is rather stable

--- clyde.py
+++ clyde.py
@@ -40,33 +40,27 @@ class Clyde(discord.Client):
         ):
             async with message.channel.typing():
                 prompt = message.content.replace(self.user.mention, "Sakoma").strip()
-                errors = 0
 
-                while True:
-                    async with httpx.AsyncClient(timeout=None) as client:
-                        response = await client.post(
-                            "http://127.0.0.1:8001/gpt", json={"prompt": prompt}
+                async with httpx.AsyncClient(timeout=None) as client:
+                    response = await client.post(
+                        "http://127.0.0.1:8001/gpt", json={"prompt": prompt}
+                    )
+                    if response.status_code == 200:
+                        gpt_message = response.json()["message"]
+                        await message.reply(gpt_message, mention_author=False)
+                    else:
+                        error_messages = [
+                            "Oops, I seem to have hit a snag! My creators are on the case, and I'll be back to normal soon.",
+                            "Woah there, I've hit a bump in the road. My creators have been notified and I'll be fixed shortly.",
+                            "Sorry, something went wrong. My creators have been notified and I'll be fixed shortly.",
+                            "Oops, I've run into a problem. But don't worry, my team is on it and I'll be back to full strength soon.",
+                            "Uh oh, I've encountered an issue. Rest assured, I have my best people working on the problem.",
+                        ]
+                        return await message.reply(
+                            random.choice(error_messages),
+                            mention_author=False,
+                            delete_after=5,
                         )
-                        if response.status_code == 200:
-                            gpt_message = response.json()["message"]
-                            await message.reply(gpt_message, mention_author=False)
-                            break
-                        else:
-                            if errors < 5:
-                                continue
-                            else:
-                                error_messages = [
-                                    "Oops, I seem to have hit a snag! My creators are on the case, and I'll be back to normal soon.",
-                                    "Woah there, I've hit a bump in the road. My creators have been notified and I'll be fixed shortly.",
-                                    "Sorry, something went wrong. My creators have been notified and I'll be fixed shortly.",
-                                    "Oops, I've run into a problem. But don't worry, my team is on it and I'll be back to full strength soon.",
-                                    "Uh oh, I've encountered an issue. Rest assured, I have my best people working on the problem.",
-                                ]
-                                return await message.reply(
-                                    random.choice(error_messages),
-                                    mention_author=False,
-                                    delete_after=5,
-                                )
 
 
 client = Clyde(max_messages=None, chunk_guilds_at_startup=False)
