14b7628b2584bd70f68abeddd4952777367c15cf
luna <71488561+piotr25691@users.noreply.github.com>
2024-02-19 12:06:00 +0100

    redone comments

--- clyde.py
+++ clyde.py
@@ -5,6 +5,7 @@ import discord
 import httpx
 from dotenv import load_dotenv
 
+# prevent leaking any tokens to cause bans or blocks
 load_dotenv()
 
 owner = 603635602809946113
@@ -17,26 +18,27 @@ class Clyde(discord.Client):
     async def on_ready(self):
         print("Clyde ready!")
 
-    async def on_message(self, message):  # ignore if:
-        forbidden = []  # block user ids from accessing Clyde here
+    async def on_message(self, message):
+        # specify user ids forbidden from using clyde, eg. 691187099570929684
+        forbidden = []
 
-        if message.author.id in forbidden:  # from blocked user
+        if message.author.id in forbidden:
             print("Ignoring message")
             return
 
-        if message.author == self.user:  # received message from itself
+        if message.author == self.user:
             print("Ignoring self message")
             return
 
-        if message.author.bot:  # received message from a bot
+        if message.author.bot:
             print("Ignoring bot message")
             return
 
-        if (  # process if:
-            self.user.mentioned_in(message)  # pinged
-            or not message.guild  # received message in a dm
+        if (
+            self.user.mentioned_in(message)
+            or not message.guild
             or message.channel.type
-            == discord.ChannelType.public_thread  # received message in a thread
+            == discord.ChannelType.public_thread
         ):
             ms = await message.reply("\u200b:clock3:", mention_author=False)
             async with message.channel.typing():
@@ -44,13 +46,15 @@ class Clyde(discord.Client):
 
                 async with httpx.AsyncClient(
                     timeout=None
-                ) as web:  # queue a response from the API
+                ) as web:
                     try:
+                        # run ai externally via an attached api
                         response = await web.post(
                             "http://127.0.0.1:8001/gpt",
                             json={"prompt": prompt, "type": "g4f"},
                         )
                     except httpx.ConnectError:
+                        # server offline error response
                         await ms.edit("\u200b:earth_africa::x:")
                         user = self.get_user(owner)
                         await user.send(
@@ -61,19 +65,19 @@ class Clyde(discord.Client):
                         return await ms.delete()
 
                     if response.status_code == 200:
+                        # correct response
                         gpt_message = response.json()["message"]
                         if len(gpt_message) <= 2000:
                             return await ms.edit(
                                 gpt_message
-                            )  # everything worked if the code got here
-
+                            )
+                        # too large response
                         await ms.edit("\u200b:scroll::warning:")
                         await asyncio.sleep(30)
                         return await ms.delete()
 
-                    user = self.get_user(
-                        owner
-                    )  # replace with ID of the owner's ID you want to send errors to
+                    # error response
+                    user = self.get_user(owner)
                     newline = "\n"
                     await ms.edit("\u200b:scroll::x:")
                     await user.send(
@@ -81,12 +85,12 @@ class Clyde(discord.Client):
                         f"Error {response.json()['code']} has occurred: {response.json()['error']}\n"
                         f"The following errors were caught:\n{newline.join(response.json()['errors'])}\n\n"
                         f"If someone else got this error, tell them to retry their request."
-                    )  # comprehensive error reports, only the owner will get those
+                    )  # only the owner id will get this
                     await asyncio.sleep(30)
                     return await ms.delete()
 
 
 client = Clyde(
     max_messages=None, chunk_guilds_at_startup=False
-)  # this speeds-up selfbot boots
-client.run(os.getenv("TOKEN"))  # be careful with posting a token here
+)
+client.run(os.getenv("TOKEN"))
