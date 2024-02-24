6abfbd4104c911c420ed9c2cc1d3904f3546e9c5
luna <71488561+piotr25691@users.noreply.github.com>
2024-02-24 09:19:57 +0100

    Fix DeprecationWarning: There is no current event loop

--- mux.py
+++ mux.py
@@ -23,7 +23,8 @@ async def _stream_subprocess(cmd, stdout_cb, stderr_cb):
 
 
 def execute(cmd, stdout_cb, stderr_cb):
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     rc = loop.run_until_complete(
         _stream_subprocess(
             cmd,
